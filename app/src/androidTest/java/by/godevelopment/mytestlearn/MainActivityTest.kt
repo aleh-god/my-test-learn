a426cef5a7a4589d526f677ca648ec95e556f316
aleh-god <aleh.god@gmail.com>
2022-05-20 22:59:00 +0300

    add: TextView test

--- app/src/androidTest/java/by/godevelopment/mytestlearn/MainActivityTest.kt
+++ app/src/androidTest/java/by/godevelopment/mytestlearn/MainActivityTest.kt
@@ -1,12 +1,17 @@
 package by.godevelopment.mytestlearn
 
+import android.app.Activity
+import androidx.test.annotation.UiThreadTest
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.core.IsNot.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,6 +19,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class MainActivityTest {
+
+    private val testText = "test"
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
@@ -32,4 +39,19 @@ class MainActivityTest {
         onView(withId(R.id.message)).check(matches(withText("null")))
     }
 
+    @Test
+    fun checkTextView() {
+        onView(withId(R.id.input_text)).perform(typeText(testText), closeSoftKeyboard())
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.message)).check(matches(withText(testText)))
+    }
+
+//    @Test
+//    fun checkToast() {
+//        onView(withId(R.id.input_text)).perform(typeText(testText), closeSoftKeyboard())
+//        onView(withId(R.id.button)).perform(click())
+//        onView(withText(testText))
+//            .inRoot(withDecorView(not(`is`(Activity().window.decorView))))
+//            .check(matches(isDisplayed()))
+//    }
 }
